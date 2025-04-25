4740de91e99d05445ff51e0d2dfbb4d3c7456831
developer-ahmed-shehta <ahmedshehta0123@gmail.com>
2025-04-25 18:28:20 +0300

    add crispy form , and bootstrip to system

--- news_paper/settings.py
+++ news_paper/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    # 3rd Party
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # local
+    'accounts',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -125,5 +130,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
-LOGIN_REDIRECT_URL = "home" # new
-LOGOUT_REDIRECT_URL = "home" # new
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
