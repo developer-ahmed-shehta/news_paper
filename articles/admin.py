a3c80de8aa21278a6e599019f8849afa3bf66b76
developer-ahmed-shehta <ahmedshehta0123@gmail.com>
2025-05-01 00:14:52 +0300

    add comment to articles and the logic

--- articles/admin.py
+++ articles/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+
 # Register your models here.
+class CommentAdmin(admin.TabularInline):
+    model = Comment
+    # list_display = ['article', 'author', 'body']
+    extra = 0
 
 
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentAdmin,
+    ]
+
     model = Article
-    list_display = ('title', 'author', 'body')
+
+    list_display = ['title', 'author', 'body']
+
+
 
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
-
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
@@ -17,3 +16,14 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse("article_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    body = models.CharField(max_length=150)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
